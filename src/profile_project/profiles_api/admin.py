fd23706855d39f2f467be2a95a007ec794eb38ee
Brady Cheng <brady31027@gmail.com>
2019-01-06 19:44:23 -0800

    add our first ApiView

--- src/profile_project/profiles_api/admin.py
+++ src/profile_project/profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'last_login', 'is_staff', 'is_superuser')
 
 # Register your models here.
+admin.site.register(models.UserProfile, UserProfileAdmin)
--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'Use HTTP methods as function(get, post, patch, put, delete)',
+            'Similar to traditional Django View',
+            'Gives most control of your app logic',
+            'It\'s mapped manually to URLs',
+        ]
+        return Response({'message': 'Hello!',
+                         'an_apiview': an_apiview})
